28b779d8abb9b9209e81ea75a21864d7974d0b8c
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2020-12-21 23:04:23 -0500

    Log to console

--- application/main/routes.py
+++ application/main/routes.py
@@ -7,11 +7,11 @@ from flask import Blueprint, render_template, request
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('error.log')
+stream_handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s: %(levelname)s: \
                               %(name)s: %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 main = Blueprint('main', __name__)
 
